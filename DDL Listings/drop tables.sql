6d4a8df8b5f11798dd35045ee7d38a787cbe9229
Harald Schütt <harald.schuett@infomotion.de>
2020-04-24 15:09:13 +0200

    DDL: Anlegen und Löschen von Tabellen des Demo Datenmodells

---
+++ DDL Listings/drop tables.sql
@@ -0,0 +1,10 @@
+/* Automatisiertes Löschen der DB-Tabellen des Demo Datenmodells
+zur Schulung "SQL Script für BW Consultants"
+per DDL CREATE Anweisungen */
+-- Harald Schütt, 24.04.2020
+
+DROP TABLE banks;
+DROP TABLE bp;
+DROP TABLE accounts;
+DROP TABLE bk_bp_accnt;
+DROP TABLE error_log;

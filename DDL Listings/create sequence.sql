1a94f3ff30d6374a7f42e8fcfeefc90b13fb7384
Harald Schütt <harald.schuett@infomotion.de>
2020-04-25 21:51:39 +0200

    select dummy ergänzt

--- DDL Listings/create sequence.sql
+++ DDL Listings/create sequence.sql
@@ -3,3 +3,5 @@ zur Nutzung innerhalb der Schulung "SQL Script für BW Consultants"
 per DDL CREATE Anweisungen */
 -- Harald Schütt, 25.04.2020
 CREATE SEQUENCE mySequence START WITH 1000 INCREMENT BY 10 NO MAXVALUE NO MINVALUE NO CYCLE;
+-- Abrufen/generieren des nächsten Wertes per nachfolgendem SQL Statement:
+--SELECT mySequence.NEXTVAL AS nextvalue FROM DUMMY;

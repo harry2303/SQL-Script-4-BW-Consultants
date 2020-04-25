7d01544a8a298687fe9be5bb47a108aa00acfb6b
Harald Schütt <harald.schuett@infomotion.de>
2020-04-25 21:24:04 +0200

    create sequence

---
+++ DDL Listings/create sequence.sql
@@ -0,0 +1,5 @@
+/* Automatisiertes Anlegen einer Sequence im Demo Datenmodell
+zur Nutzung innerhalb der Schulung "SQL Script für BW Consultants"
+per DDL CREATE Anweisungen */
+-- Harald Schütt, 25.04.2020
+CREATE SEQUENCE mySequence START WITH 1000 INCREMENT BY 10 NO MAXVALUE NO MINVALUE NO CYCLE;

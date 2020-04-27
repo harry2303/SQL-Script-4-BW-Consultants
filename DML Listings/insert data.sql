edd5d45539760bf274e5fc0c8878ca66e8b1efa0
Harald Schütt <harald.schuett@infomotion.de>
2020-04-27 18:45:54 +0200

    insert data

--- DML Listings/insert data.sql
+++ DML Listings/insert data.sql
@@ -1,109 +1,121 @@
-/* Automatisiertes Einfügen von Daten zu Banken in das Demo Datenmodells
+/* Automatisiertes Einfügen von Daten in das Demo Datenmodells
 zur Schulung "SQL Script 4 BW Consultants" */
 -- Harald Schütt, 27.04.2020
 --
 -- Daten für Tabelle banks
-insert into banks (DXIADEBBXXX, 'Helaba Berlin', 60311, 'Frankfurt am Main');
-insert into banks (DLGHDEB1XXX, 'Eurocity Bank', 60311, 'Frankfurt am Main');
-insert into banks (DGZFDEFFBER, 'DekaBank', 60040, 'Frankfurt am Main');
-insert into banks (MACODEB1XXX, 'Westend Bank', 60599, 'Frankfurt am Main');
-insert into banks (BIWBDE33XXX, 'flatex Bank Frankfurt', 60327, 'Frankfurt am Main');
-insert into banks (ESSEDEFFHAM, 'SEB Hamburg', 60313, 'Frankfurt am Main');
-insert into banks (BIWBDE33303, 'flatex Bank Frankfurt', 60327, 'Frankfurt am Main');
-insert into banks (GABKDE3EXXX, 'NIBC Bank Deutschland', 60311, 'Frankfurt am Main');
-insert into banks (BNPADEFFXXX, 'BNP Paribas Ndl Deutschland', 60327, 'Frankfurt am Main');
-insert into banks (MARKDEF1500, 'BBk Filiale Frankfurt Main', 60047, 'Frankfurt am Main');
-insert into banks (PBNKDEFFXXX, 'Postbank Ndl DB PFK', 60288, 'Frankfurt am Main');
-insert into banks (ESSEDE5FXXX, 'DSK Hyp', 60313, 'Frankfurt am Main');
-insert into banks (AKBKDEFFXXX, 'AKBANK', 60310, 'Frankfurt am Main');
-insert into banks (AARBDE5W500, 'Aareal Bank', 60284, 'Frankfurt am Main');
-insert into banks (INGDDEFFXXX, 'ING-DiBa Frankfurt am Main', 60628, 'Frankfurt am Main');
-insert into banks (DEGUDEFFXXX, 'Degussa Bank Frankfurt Main', 60605, 'Frankfurt am Main');
-insert into banks (BOFADEFXXXX, 'Bankamerica', 60311, 'Frankfurt am Main');
-insert into banks (BOFADEFXVAM, 'Bank of America', 60311, 'Frankfurt am Main');
-insert into banks (WUIDDEF1XXX, 'WUIB - Branch Germany', 60486, 'Frankfurt am Main');
-insert into banks (SEZDDEF1XXX, 'Sberbank Direct Frankfurt', 60594, 'Frankfurt am Main');
-insert into banks (BHFBDEFF500, 'ODDO BHF Frankfurt Main', 60323, 'Frankfurt am Main');
-insert into banks (KFWIDEFFXXX, 'KfW Frankfurt', 60046, 'Frankfurt am Main');
-insert into banks (LAREDEFFXXX, 'Landwirtschaftl Rentenbank', 60014, 'Frankfurt am Main');
-insert into banks (FBHLDEFFXXX, 'Credit Europe Bank Ffm', 60050, 'Frankfurt am Main');
-insert into banks (BCITDEFFXXX, 'Intesa Sanpaolo Frankfurt', 60011, 'Frankfurt am Main');
-insert into banks (INGBDEFFXXX, 'ING Bank Frankfurt', 60486, 'Frankfurt am Main');
-insert into banks (FFBKDEFFKRN, 'FIL Fondsbank Frankfurt', 60311, 'Frankfurt am Main');
-insert into banks (HCSEDEF1XXX, 'HCBE Frankfurt am Main', 60327, 'Frankfurt am Main');
-insert into banks (HKBBDEF1FRA, 'HKB Bank Frankfurt am Main', 60528, 'Frankfurt am Main');
-insert into banks (PARBDEFFXXX, 'BNP PARIBAS Sec Serv', 60003, 'Frankfurt am Main');
-insert into banks (DWPBDEFFXXX, 'dwpbank', 60487, 'Frankfurt am Main');
-insert into banks (ESBKDEFFXXX, 'DenizBank Frankfurt', 60329, 'Frankfurt am Main');
-insert into banks (TRODDEF1XXX, 'Triodos Bank Deutschland', 60326, 'Frankfurt am Main');
-insert into banks (SCFBDE33XXX, 'Santander Bank Frankfurt', 60325, 'Frankfurt am Main');
-insert into banks (ABOCDEFFXXX, 'Agricultural Bank China FRA', 60325, 'Frankfurt am Main');
-insert into banks (COBADEFFXXX, 'Commerzbank Frankfurt Main', 60005, 'Frankfurt am Main');
-insert into banks (HELADEFFXXX, 'Ld Bk Hess-Thür Gz Ffm', 60297, 'Frankfurt am Main');
-insert into banks (HELADEF1822, 'Frankfurter Spk 1822direkt', 60255, 'Frankfurt am Main');
-insert into banks (DGZFDEFFXXX, 'DekaBank Frankfurt', 60329, 'Frankfurt am Main');
-insert into banks (GENODE55XXX, 'DZ Bank', 60274, 'Frankfurt am Main');
-insert into banks (GENODEFFXXX, 'DZ BANK', 60265, 'Frankfurt am Main');
-insert into banks (DEUTDEFFXXX, 'Deutsche Bank Frankfurt', 60254, 'Frankfurt am Main');
-insert into banks (DEUTDEFFSIP, 'Deutsche Bank F-SIP', 60329, 'Frankfurt am Main');
-insert into banks (DEUTDEDBFRA, 'DB PFK (Deutsche Bank PGK)', 60254, 'Frankfurt am Main');
-insert into banks (DEUTDE5XXXX, 'Deutsche Bank Europe', 60486, 'Frankfurt am Main');
-insert into banks (DRESDEFFXXX, 'Commerzbank Frankfurt', 60613, 'Frankfurt am Main');
-insert into banks (GENODEF1S12, 'Sparda-Bank Hessen', 60052, 'Frankfurt am Main');
-insert into banks (DAAEDED1007, 'apoBank Frankfurt Main', 60061, 'Frankfurt am Main');
-insert into banks (ICBKDEFFXXX, 'ICBC Frankfurt Main', 60322, 'Frankfurt am Main');
-insert into banks (DVKBDEFFXXX, 'DVB Frankfurt Main', 60040, 'Frankfurt am Main');
-insert into banks (AUSKDEFFXXX, 'AKA Ausfuhrkredit Frankfurt', 60001, 'Frankfurt am Main');
-insert into banks (NATXDEFFXXX, 'NATIXIS Frankfurt am Main', 60322, 'Frankfurt am Main');
-insert into banks (SCBLDEFXXXX, 'SCB FFM', 60325, 'Frankfurt am Main');
-insert into banks (FBGADEF1XXX, 'FBG (D)', 60311, 'Frankfurt am Main');
-insert into banks (CHASDEFXXXX, 'J.P. Morgan Frankfurt', 60311, 'Frankfurt am Main');
-insert into banks (MAIFDEFFXXX, 'MAINFIRST BANK', 60596, 'Frankfurt am Main');
-insert into banks (DELBDE33XXX, 'Bethmann Bank Frankfurt', 60329, 'Frankfurt am Main');
-insert into banks (CRESDE55XXX, 'CSD Frankfurt Main', 60310, 'Frankfurt am Main');
-insert into banks (COMMDEFFXXX, 'Bank of Communications', 60311, 'Frankfurt am Main');
-insert into banks (DOBADEF1XXX, 'VTB Direktbank', 60325, 'Frankfurt am Main');
-insert into banks (BPKODEFFXXX, 'PKO Bank Polski Frankfurt', 60311, 'Frankfurt am Main');
-insert into banks (NBPADEFFXXX, 'Naba Frankfurt Main', 60313, 'Frankfurt am Main');
-insert into banks (MEFIDEMM501, 'Merck Finck Privatbankiers', 60015, 'Frankfurt am Main');
-insert into banks (UBSWDEFFXXX, 'UBS Europe', 60079, 'Frankfurt am Main');
-insert into banks (ICBVDEFFXXX, 'Vietinbank', 60322, 'Frankfurt am Main');
-insert into banks (FFVBDEFFXXX, 'Frankfurter Volksbank', 60008, 'Frankfurt am Main');
-insert into banks (GENODE51FHC, 'VB Höchst a.M.Frankfurt alt', 65929, 'Frankfurt am Main');
-insert into banks (GENODE51FGH, 'VB Griesheim Ffm -alt-', 65933, 'Frankfurt am Main');
-insert into banks (RABODEFFTAR, 'Rabobank Frankfurt Main', 60486, 'Frankfurt am Main');
-insert into banks (RABODEFFDIR, 'RaboDirect Frankfurt Main', 60486, 'Frankfurt am Main');
-insert into banks (PRCBDEFFXXX, 'ProCredit Bank, Frankfurt', 60486, 'Frankfurt am Main');
-insert into banks (CITIDEFFXXX, 'CEP Germany Branch', 60038, 'Frankfurt am Main');
-insert into banks (SMHBDEFFXXX, 'UBS Europe', 60020, 'Frankfurt am Main');
-insert into banks (INVODEF2XXX, 'Kommunalkredit Deutschland', 60313, 'Frankfurt am Main');
-insert into banks (HVBKDEFFXXX, 'WOORI BANK', 60308, 'Frankfurt am Main');
-insert into banks (CAIXDEFFXXX, 'CAIXABANK Zndl Frankfurt', 60311, 'Frankfurt am Main');
-insert into banks (HAUKDEFFXXX, 'Hauck & Aufhäuser', 60010, 'Frankfurt am Main');
-insert into banks (ABCADEFFXXX, 'ABC International Bank Ffm', 60311, 'Frankfurt am Main');
-insert into banks (MSPCDEF1XXX, 'Morgan Stanley Bank Int', 60016, 'Frankfurt am Main');
-insert into banks (ISBKDEFXXXX, 'Isbank Frankfurt Main', 60022, 'Frankfurt am Main');
-insert into banks (METZDEFFXXX, 'Bankhaus Metzler Frankfurt', 60329, 'Frankfurt am Main');
-insert into banks (KTAGDEFFXXX, 'KT Bank Frankfurt', 60327, 'Frankfurt am Main');
-insert into banks (BARCDEFFXXX, 'Barclays Bank IE, Frankfurt', 60310, 'Frankfurt am Main');
-insert into banks (PCBCDEFFXXX, 'CCB Frankfurt', 60078, 'Frankfurt am Main');
-insert into banks (OWHBDEFFXXX, 'VTB Bank (Europe) Ffm', 60325, 'Frankfurt am Main');
-insert into banks (HYVEDEMM430, 'UniCredit Bank-HypoVereinbk', 60003, 'Frankfurt am Main');
-insert into banks (BSCHDEFFXXX, 'Banco Santander Ffm', 60003, 'Frankfurt am Main');
-insert into banks (BCMADEFFXXX, 'Attijariwafa bank Frankfurt', 60329, 'Frankfurt am Main');
-insert into banks (PICTDEFFXXX, 'Pictet & Cie Europe Ffm', 60311, 'Frankfurt am Main');
-insert into banks (FTSBDEFAXXX, 'ABN AMRO Bank', 60325, 'Frankfurt am Main');
+insert into banks ('DXIADEBBXXX', 'Helaba Berlin', 60311, 'Frankfurt am Main');
+insert into banks ('DLGHDEB1XXX', 'Eurocity Bank', 60311, 'Frankfurt am Main');
+insert into banks ('DGZFDEFFBER', 'DekaBank', 60040, 'Frankfurt am Main');
+insert into banks ('MACODEB1XXX', 'Westend Bank', 60599, 'Frankfurt am Main');
+insert into banks ('BIWBDE33XXX', 'flatex Bank Frankfurt', 60327, 'Frankfurt am Main');
+insert into banks ('ESSEDEFFHAM', 'SEB Hamburg', 60313, 'Frankfurt am Main');
+insert into banks ('BIWBDE33303', 'flatex Bank Frankfurt', 60327, 'Frankfurt am Main');
+insert into banks ('GABKDE3EXXX', 'NIBC Bank Deutschland', 60311, 'Frankfurt am Main');
+insert into banks ('BNPADEFFXXX', 'BNP Paribas Ndl Deutschland', 60327, 'Frankfurt am Main');
+insert into banks ('MARKDEF1500', 'BBk Filiale Frankfurt Main', 60047, 'Frankfurt am Main');
+insert into banks ('PBNKDEFFXXX', 'Postbank Ndl DB PFK', 60288, 'Frankfurt am Main');
+insert into banks ('ESSEDE5FXXX', 'DSK Hyp', 60313, 'Frankfurt am Main');
+insert into banks ('AKBKDEFFXXX', 'AKBANK', 60310, 'Frankfurt am Main');
+insert into banks ('AARBDE5W500', 'Aareal Bank', 60284, 'Frankfurt am Main');
+insert into banks ('INGDDEFFXXX', 'ING-DiBa Frankfurt am Main', 60628, 'Frankfurt am Main');
+insert into banks ('DEGUDEFFXXX', 'Degussa Bank Frankfurt Main', 60605, 'Frankfurt am Main');
+insert into banks ('BOFADEFXXXX', 'Bankamerica', 60311, 'Frankfurt am Main');
+insert into banks ('BOFADEFXVAM', 'Bank of America', 60311, 'Frankfurt am Main');
+insert into banks ('WUIDDEF1XXX', 'WUIB - Branch Germany', 60486, 'Frankfurt am Main');
+insert into banks ('SEZDDEF1XXX', 'Sberbank Direct Frankfurt', 60594, 'Frankfurt am Main');
+insert into banks ('BHFBDEFF500', 'ODDO BHF Frankfurt Main', 60323, 'Frankfurt am Main');
+insert into banks ('KFWIDEFFXXX', 'KfW Frankfurt', 60046, 'Frankfurt am Main');
+insert into banks ('LAREDEFFXXX', 'Landwirtschaftl Rentenbank', 60014, 'Frankfurt am Main');
+insert into banks ('FBHLDEFFXXX', 'Credit Europe Bank Ffm', 60050, 'Frankfurt am Main');
+insert into banks ('BCITDEFFXXX', 'Intesa Sanpaolo Frankfurt', 60011, 'Frankfurt am Main');
+insert into banks ('INGBDEFFXXX', 'ING Bank Frankfurt', 60486, 'Frankfurt am Main');
+insert into banks ('FFBKDEFFKRN', 'FIL Fondsbank Frankfurt', 60311, 'Frankfurt am Main');
+insert into banks ('HCSEDEF1XXX', 'HCBE Frankfurt am Main', 60327, 'Frankfurt am Main');
+insert into banks ('HKBBDEF1FRA', 'HKB Bank Frankfurt am Main', 60528, 'Frankfurt am Main');
+insert into banks ('PARBDEFFXXX', 'BNP PARIBAS Sec Serv', 60003, 'Frankfurt am Main');
+insert into banks ('DWPBDEFFXXX', 'dwpbank', 60487, 'Frankfurt am Main');
+insert into banks ('ESBKDEFFXXX', 'DenizBank Frankfurt', 60329, 'Frankfurt am Main');
+insert into banks ('TRODDEF1XXX', 'Triodos Bank Deutschland', 60326, 'Frankfurt am Main');
+insert into banks ('SCFBDE33XXX', 'Santander Bank Frankfurt', 60325, 'Frankfurt am Main');
+insert into banks ('ABOCDEFFXXX', 'Agricultural Bank China FRA', 60325, 'Frankfurt am Main');
+insert into banks ('COBADEFFXXX', 'Commerzbank Frankfurt Main', 60005, 'Frankfurt am Main');
+insert into banks ('HELADEFFXXX', 'Ld Bk Hess-Thür Gz Ffm', 60297, 'Frankfurt am Main');
+insert into banks ('HELADEF1822', 'Frankfurter Spk 1822direkt', 60255, 'Frankfurt am Main');
+insert into banks ('DGZFDEFFXXX', 'DekaBank Frankfurt', 60329, 'Frankfurt am Main');
+insert into banks ('GENODE55XXX', 'DZ Bank', 60274, 'Frankfurt am Main');
+insert into banks ('GENODEFFXXX', 'DZ BANK', 60265, 'Frankfurt am Main');
+insert into banks ('DEUTDEFFXXX', 'Deutsche Bank Frankfurt', 60254, 'Frankfurt am Main');
+insert into banks ('DEUTDEFFSIP', 'Deutsche Bank F-SIP', 60329, 'Frankfurt am Main');
+insert into banks ('DEUTDEDBFRA', 'DB PFK (Deutsche Bank PGK)', 60254, 'Frankfurt am Main');
+insert into banks ('DEUTDE5XXXX', 'Deutsche Bank Europe', 60486, 'Frankfurt am Main');
+insert into banks ('DRESDEFFXXX', 'Commerzbank Frankfurt', 60613, 'Frankfurt am Main');
+insert into banks ('GENODEF1S12', 'Sparda-Bank Hessen', 60052, 'Frankfurt am Main');
+insert into banks ('DAAEDED1007', 'apoBank Frankfurt Main', 60061, 'Frankfurt am Main');
+insert into banks ('ICBKDEFFXXX', 'ICBC Frankfurt Main', 60322, 'Frankfurt am Main');
+insert into banks ('DVKBDEFFXXX', 'DVB Frankfurt Main', 60040, 'Frankfurt am Main');
+insert into banks ('AUSKDEFFXXX', 'AKA Ausfuhrkredit Frankfurt', 60001, 'Frankfurt am Main');
+insert into banks ('NATXDEFFXXX', 'NATIXIS Frankfurt am Main', 60322, 'Frankfurt am Main');
+insert into banks ('SCBLDEFXXXX', 'SCB FFM', 60325, 'Frankfurt am Main');
+insert into banks ('FBGADEF1XXX', 'FBG (D)', 60311, 'Frankfurt am Main');
+insert into banks ('CHASDEFXXXX', 'J.P. Morgan Frankfurt', 60311, 'Frankfurt am Main');
+insert into banks ('MAIFDEFFXXX', 'MAINFIRST BANK', 60596, 'Frankfurt am Main');
+insert into banks ('DELBDE33XXX', 'Bethmann Bank Frankfurt', 60329, 'Frankfurt am Main');
+insert into banks ('CRESDE55XXX', 'CSD Frankfurt Main', 60310, 'Frankfurt am Main');
+insert into banks ('COMMDEFFXXX', 'Bank of Communications', 60311, 'Frankfurt am Main');
+insert into banks ('DOBADEF1XXX', 'VTB Direktbank', 60325, 'Frankfurt am Main');
+insert into banks ('BPKODEFFXXX', 'PKO Bank Polski Frankfurt', 60311, 'Frankfurt am Main');
+insert into banks ('NBPADEFFXXX', 'Naba Frankfurt Main', 60313, 'Frankfurt am Main');
+insert into banks ('MEFIDEMM501', 'Merck Finck Privatbankiers', 60015, 'Frankfurt am Main');
+insert into banks ('UBSWDEFFXXX', 'UBS Europe', 60079, 'Frankfurt am Main');
+insert into banks ('ICBVDEFFXXX', 'Vietinbank', 60322, 'Frankfurt am Main');
+insert into banks ('FFVBDEFFXXX', 'Frankfurter Volksbank', 60008, 'Frankfurt am Main');
+insert into banks ('GENODE51FHC', 'VB Höchst a.M.Frankfurt alt', 65929, 'Frankfurt am Main');
+insert into banks ('GENODE51FGH', 'VB Griesheim Ffm -alt-', 65933, 'Frankfurt am Main');
+insert into banks ('RABODEFFTAR', 'Rabobank Frankfurt Main', 60486, 'Frankfurt am Main');
+insert into banks ('RABODEFFDIR', 'RaboDirect Frankfurt Main', 60486, 'Frankfurt am Main');
+insert into banks ('PRCBDEFFXXX', 'ProCredit Bank, Frankfurt', 60486, 'Frankfurt am Main');
+insert into banks ('CITIDEFFXXX', 'CEP Germany Branch', 60038, 'Frankfurt am Main');
+insert into banks ('SMHBDEFFXXX', 'UBS Europe', 60020, 'Frankfurt am Main');
+insert into banks ('INVODEF2XXX', 'Kommunalkredit Deutschland', 60313, 'Frankfurt am Main');
+insert into banks ('HVBKDEFFXXX', 'WOORI BANK', 60308, 'Frankfurt am Main');
+insert into banks ('CAIXDEFFXXX', 'CAIXABANK Zndl Frankfurt', 60311, 'Frankfurt am Main');
+insert into banks ('HAUKDEFFXXX', 'Hauck & Aufhäuser', 60010, 'Frankfurt am Main');
+insert into banks ('ABCADEFFXXX', 'ABC International Bank Ffm', 60311, 'Frankfurt am Main');
+insert into banks ('MSPCDEF1XXX', 'Morgan Stanley Bank Int', 60016, 'Frankfurt am Main');
+insert into banks ('ISBKDEFXXXX', 'Isbank Frankfurt Main', 60022, 'Frankfurt am Main');
+insert into banks ('METZDEFFXXX', 'Bankhaus Metzler Frankfurt', 60329, 'Frankfurt am Main');
+insert into banks ('KTAGDEFFXXX', 'KT Bank Frankfurt', 60327, 'Frankfurt am Main');
+insert into banks ('BARCDEFFXXX', 'Barclays Bank IE, Frankfurt', 60310, 'Frankfurt am Main');
+insert into banks ('PCBCDEFFXXX', 'CCB Frankfurt', 60078, 'Frankfurt am Main');
+insert into banks ('OWHBDEFFXXX', 'VTB Bank (Europe) Ffm', 60325, 'Frankfurt am Main');
+insert into banks ('HYVEDEMM430', 'UniCredit Bank-HypoVereinbk', 60003, 'Frankfurt am Main');
+insert into banks ('BSCHDEFFXXX', 'Banco Santander Ffm', 60003, 'Frankfurt am Main');
+insert into banks ('BCMADEFFXXX', 'Attijariwafa bank Frankfurt', 60329, 'Frankfurt am Main');
+insert into banks ('PICTDEFFXXX', 'Pictet & Cie Europe Ffm', 60311, 'Frankfurt am Main');
+insert into banks ('FTSBDEFAXXX', 'ABN AMRO Bank', 60325, 'Frankfurt am Main');
 --
 -- Daten für Tabelle bp
-insert into bp (BPSequ.nextval, 'Jonah', 'Beams', );
-insert into bp (BPSequ.nextval, 'North', 'Bryett', );
-insert into bp (BPSequ.nextval, 'Brietta', 'Nerney', );
-insert into bp (BPSequ.nextval, 'Ailis', 'Lampke', );
-insert into bp (BPSequ.nextval, 'Frayda', 'Tolle', );
-insert into bp (BPSequ.nextval, 'Ferdy', 'Duester', );
-insert into bp (BPSequ.nextval, 'Olag', 'Wemes', );
-insert into bp (BPSequ.nextval, 'Alfredo', 'Orth', );
-insert into bp (BPSequ.nextval, 'Sharity', 'Lodwick', );
-insert into bp (BPSequ.nextval, 'Agnese', 'Fideler', );
-insert into bp (BPSequ.nextval, 'Doralin', 'Fernley', );
+insert into bp (BPSequ.nextval, 'Jonah', 'Beams', ); -- generate primary key by sequence BPSequ
+insert into bp (BPSequ.nextval, 'North', 'Bryett', ); -- generate primary key by sequence BPSequ
+insert into bp (BPSequ.nextval, 'Brietta', 'Nerney', ); -- generate primary key by sequence BPSequ
+insert into bp (BPSequ.nextval, 'Ailis', 'Lampke', ); -- generate primary key by sequence BPSequ
+insert into bp (BPSequ.nextval, 'Frayda', 'Tolle', ); -- generate primary key by sequence BPSequ
+insert into bp (BPSequ.nextval, 'Ferdy', 'Duester', ); -- generate primary key by sequence BPSequ
+insert into bp (BPSequ.nextval, 'Olag', 'Wemes', ); -- generate primary key by sequence BPSequ
+insert into bp (BPSequ.nextval, 'Alfredo', 'Orth', ); -- generate primary key by sequence BPSequ
+insert into bp (BPSequ.nextval, 'Sharity', 'Lodwick', ); -- generate primary key by sequence BPSequ
+insert into bp (BPSequ.nextval, 'Agnese', 'Fideler', ); -- generate primary key by sequence BPSequ
+insert into bp (BPSequ.nextval, 'Doralin', 'Fernley', ); -- generate primary key by sequence BPSequ
 --
+-- Daten für Tabelle Accounts
+insert into accounts (AccntsSequ.nextval, 'DXIADEBBXXX', CURRENT_DATE); -- generate primary key by sequence AccntsSequ
+insert into accounts (AccntsSequ.nextval, 'MEFIDEMM501', CURRENT_DATE); -- generate primary key by sequence AccntsSequ
+insert into accounts (AccntsSequ.nextval, 'RABODEFFTAR', CURRENT_DATE); -- generate primary key by sequence AccntsSequ
+insert into accounts (AccntsSequ.nextval, 'DRESDEFFXXX', CURRENT_DATE); -- generate primary key by sequence AccntsSequ
+insert into accounts (AccntsSequ.nextval, 'HELADEFFXXX', CURRENT_DATE); -- generate primary key by sequence AccntsSequ
+insert into accounts (AccntsSequ.nextval, 'FBGADEF1XXX', CURRENT_DATE); -- generate primary key by sequence AccntsSequ
+insert into accounts (AccntsSequ.nextval, 'MARKDEF1500', CURRENT_DATE); -- generate primary key by sequence AccntsSequ
+insert into accounts (AccntsSequ.nextval, 'AUSKDEFFXXX', CURRENT_DATE); -- generate primary key by sequence AccntsSequ
+insert into accounts (AccntsSequ.nextval, 'ICBVDEFFXXX', CURRENT_DATE); -- generate primary key by sequence AccntsSequ
+insert into accounts (AccntsSequ.nextval, 'SMHBDEFFXXX', CURRENT_DATE); -- generate primary key by sequence AccntsSequ
+insert into accounts (AccntsSequ.nextval, 'ESSEDEFFHAM', CURRENT_DATE); -- generate primary key by sequence AccntsSequ

75b1a72850816c24bcc23be7a437fb9e1e009b53
Harald Schuett <h.schuett@web.de>
2020-04-28 13:05:19 +0000

    mod installcheck

--- DML Listings/check installation.sql
+++ DML Listings/check installation.sql
@@ -1,8 +1,8 @@
 /* Prüfung der Installation des Demo Datenmodells
    und der generierten Menge an Testdaten*/
--- Harald Schütt, 23.04.2020
+-- Harald Schütt, 28.04.2020
 
-select 'Banken', count(*) as Istmenge, '90'  as Sollmenge from banks    union
-select 'Kunden', count(*) as Istmenge, '11'  as Sollmenge from bp       union
-select 'Konten', count(*) as Istmenge, '11'  as Sollmenge from accounts union
-select 'Saldos', count(*) as Istmenge, '110' as Sollmenge from saldos;
+select 'banks' as Tabelle, count(*) as Istmenge, '90'  as Sollmenge from banks    union
+select 'bp' as Tabelle, count(*) as Istmenge, '11'  as Sollmenge from bp       union
+select 'accounts' as Tabelle, count(*) as Istmenge, '11'  as Sollmenge from accounts union
+select 'saldos' as Tabelle, count(*) as Istmenge, '110' as Sollmenge from saldos;

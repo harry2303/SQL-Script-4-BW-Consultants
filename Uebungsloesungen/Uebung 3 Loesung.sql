012b04679e4887ebe3d62f0f5ad7e0c87487b46d
Harald Schütt <harald.schuett@infomotion.de>
2020-05-12 08:46:53 +0200

    add solution for exercise 3

---
+++ Uebungsloesungen/Uebung 3 Loesung.sql
@@ -0,0 +1,15 @@
+-- Übung 3: SQL SET Operator anwenden
+-- Musterlösungen für alle Teilaufgaben der Übung
+/*************************************************************/
+--Ermittlung aller BICs aus der Tabelle banks, ohne Konto in der Tabelle accounts
+SELECT bic FROM banks
+EXCEPT
+SELECT bnk_bic AS bic FROM accounts;
+
+-- INTERSECT über das Feld Kontonr. der Tabellen accounts und saldos
+SELECT id FROM accounts
+INTERSECT
+SELECT accnt_id AS id FROM saldos;
+-- INNER JOIN ... ON ...
+SELECT a.id FROM accounts a INNER JOIN saldos s
+ON a.id = s.accnt_id;

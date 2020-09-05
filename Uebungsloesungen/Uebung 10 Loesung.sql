4caa42fb8fb06e16752c029c23e4db1d3037861f
Harald Schütt <harald.schuett@infomotion.de>
2020-09-05 07:01:59 +0200

    Ergänzung: Not NULL

--- Uebungsloesungen/Uebung 10 Loesung.sql
+++ Uebungsloesungen/Uebung 10 Loesung.sql
@@ -8,7 +8,7 @@ CREATE OR REPLACE FUNCTION UDF_DATE_CALMONTH (IN iv_date DATE)
             SQL SECURITY INVOKER AS
 BEGIN
 DECLARE lv_date DATE;
-lv_date = TRIM(:iv_date);--removing leading and trailing spaces
+lv_date = TRIM(IFNULL(:iv_date,''));--Not Null, removing leading and trailing spaces
 rv_calmonth = TO_NVARCHAR(:lv_date, 'YYYYMM');--calmonth format
 END;
 

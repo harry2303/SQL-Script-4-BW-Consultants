31ea54cbea01c327ff9cf4d16b05778ab99cc5a3
Harald Schütt <harald.schuett@infomotion.de>
2020-09-07 12:46:41 +0200

    Optimierungen der Lösung

--- Uebungsloesungen/Uebung 11 Loesung.sql
+++ Uebungsloesungen/Uebung 11 Loesung.sql
@@ -3,6 +3,7 @@
 /*************************************************************/
 --Lösungsvorschlag:
 CREATE OR REPLACE PROCEDURE UPS_TRACNTS (
+            IN iv_id    INT,
             IN iv_accnt INT       DEFAULT 600300,
             IN iv_bic VARCHAR(11) DEFAULT 'RABODEFFTAR',
             IN iv_date DATE       DEFAULT '00000000',
@@ -22,7 +23,7 @@ BEGIN
 --SELECT CURRENT_DATE "aktuelles Datum" FROM DUMMY;
 
 -- ------------------ DECLARE & INITIALIZE VARIABLE
-DECLARE lv_sequint INT = 0;
+DECLARE lv_sequid INT;
 DECLARE lv_accnt INT = 600300;
 DECLARE lv_bic VARCHAR(11) = 'RABODEFFTAR';
 DECLARE lv_date DATE = CURRENT_DATE;
@@ -44,20 +45,22 @@ lv_amnt = :iv_amnt;
 -- UPSERT transactions VALUES (...) WITH PRIMARY KEY;
 
 --Neuen Primärschlüssel mit Sequenz TrnsSequ per NEXTVAL erzeugen
-SELECT TrnsSequ.nextval INTO lv_sequint FROM dummy; --Wertzuweisung an Skalar Variable in SELECT
-UPSERT transactions VALUES(:lv_sequint, :lv_accnt, :lv_bic, :lv_date, :lv_curr, :lv_amnt) WITH PRIMARY KEY;
+SELECT IFNULL(:iv_id,TrnsSequ.nextval) INTO lv_sequid FROM dummy; --Wertzuweisung an Skalar Variable per SELECT
+UPSERT transactions VALUES(:lv_sequid, :lv_accnt, :lv_bic, :lv_date, :lv_curr, :lv_amnt) WITH PRIMARY KEY;
 
 --Geänderten Datensatz an Tabellenvariable zuweisen
---SELECT TrnsSequ.currval INTO lv_sequint FROM dummy; --aktuellen Wert der Sequence TrnsSequ abfragen
-ot_newrec = SELECT * FROM transactions WHERE ID = :lv_sequint;
+--SELECT TrnsSequ.currval INTO lv_sequid FROM dummy; --aktuellen Wert der Sequence TrnsSequ abfragen
+ot_newrec = SELECT * FROM transactions WHERE id = :lv_sequid;
 
 -- ------------------ RETURN RESULT
 ot_result = SELECT * FROM transactions;
 ot_msg = SELECT 'I' MSGTY, 'Transaktion wurde angepasst' MSG_TEXT FROM dummy;
-
 END;
 
 
 
 -- Aufruf der Procedure mit IN und OUT Parametern (hier: teilweise gebunden an Konsolenein-/ausgabe)
-CALL UPS_TRACNTS(600300, 'RABODEFFTAR', CURRENT_DATE, 'EUR', iv_amnt => ?, ot_newrec => ?, ot_result => ?, ot_msg => ?);
+-- Neuen Datensatz erzeugen --> Param. ID = NULL
+CALL UPS_TRACNTS(NULL, 600300, 'RABODEFFTAR', CURRENT_DATE, 'EUR', iv_amnt => ?, ot_newrec => ?, ot_result => ?, ot_msg => ?);
+--Datensatz ändern --> Param. ID = vorhandene Datensatz ID
+CALL UPS_TRACNTS(5044, 600300, 'RABODEFFTAR', CURRENT_DATE, 'EUR', iv_amnt => ?, ot_newrec => ?, ot_result => ?, ot_msg => ?);

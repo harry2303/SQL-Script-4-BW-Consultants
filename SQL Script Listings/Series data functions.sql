b5aefcb9aed846ea0caa5d6b3b5ebf4fc705361b
Harald Schütt <harald.schuett@infomotion.de>
2020-10-12 08:05:37 +0200

    time series fkt. ergänzt

--- SQL Script Listings/Series data functions.sql
+++ SQL Script Listings/Series data functions.sql
@@ -8,9 +8,12 @@ SELECT GENERATED_PERIOD_START AS "Monatserster",
        ADD_DAYS(GENERATED_PERIOD_END, -1) AS "Monatsletzter"
     FROM SERIES_GENERATE_DATE('INTERVAL 1 MONTH', '2020-01-01', '2021-01-01');
 
---Erzeugen von Perioden (Fiscper3)
+--Erzeugen von Perioden (Fiscper3) per SERIES_GENERATE Fkt. und formatieren per LPAD
 SELECT LPAD(GENERATED_PERIOD_START,3,0) AS fiscper3
     FROM SERIES_GENERATE_TINYINT(1, 1, 17);
+--Perioden per SERIES_GENERATE Fkt. erzeugen und formatieren per LPAD
+SELECT LPAD(GENERATED_PERIOD_END,3,0) AS fiscper3
+    FROM SERIES_GENERATE_INTEGER(1, 0, 16);
 
 --Alle Tage eines Jahres erzeugen lassen    
 SELECT to_nvarchar(GENERATED_PERIOD_START,'YYYYMMDD') AS calmonth

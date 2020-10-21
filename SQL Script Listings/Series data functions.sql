a0b5d64d0aedb21639e8df684fa9a62fce660a66
Harald Schütt <harald.schuett@infomotion.de>
2020-10-21 22:58:39 +0200

    Modifications "series generate functions" sqls

--- SQL Script Listings/Series data functions.sql
+++ SQL Script Listings/Series data functions.sql
@@ -1,12 +1,14 @@
-/* Daten generieren per Series data functions */
+/* Datenreihen generieren per Series data functions */
+SELECT *
+    FROM SERIES_GENERATE_INTEGER(2, 1, 23);
 
 SELECT *
-    FROM SERIES_GENERATE_DATE('INTERVAL 1 MONTH', '2020-01-01', '2021-01-01');
+    FROM SERIES_GENERATE_DATE('INTERVAL 1 MONTH', date'2020-01-01', date'2021-01-01');
 
 --Erzeugen von "Monatsscheiben" per SERIES_GENERATE_DATE
 SELECT GENERATED_PERIOD_START AS "Monatserster",
        ADD_DAYS(GENERATED_PERIOD_END, -1) AS "Monatsletzter"
-    FROM SERIES_GENERATE_DATE('INTERVAL 1 MONTH', '2020-01-01', '2021-01-01');
+    FROM SERIES_GENERATE_DATE('INTERVAL 1 MONTH', date'2020-01-01', date'2021-01-01');
 
 --Erzeugen von Perioden (Fiscper3) per SERIES_GENERATE Fkt. und formatieren per LPAD
 SELECT LPAD(GENERATED_PERIOD_START,3,0) AS fiscper3
@@ -17,10 +19,9 @@ SELECT LPAD(GENERATED_PERIOD_END,3,0) AS fiscper3
 
 --Alle Tage eines Jahres erzeugen lassen    
 SELECT to_nvarchar(GENERATED_PERIOD_START,'YYYYMMDD') AS calmonth
-    FROM SERIES_GENERATE_DATE('INTERVAL 1 DAY', '2020-01-01', '2021-01-01');
+    FROM SERIES_GENERATE_DATE('INTERVAL 1 DAY', date'2020-01-01', date'2021-01-01');
 
 --Erzeugen von "Tagesscheiben" für 1 Jahr
 SELECT GENERATED_PERIOD_START AS from_date,
        GENERATED_PERIOD_END AS to_date
-    FROM SERIES_GENERATE_DATE('INTERVAL 1 DAY', '2020-01-01', '2020-12-31')
-
+    FROM SERIES_GENERATE_DATE('INTERVAL 1 DAY', date'2020-01-01', date'2020-12-31')
